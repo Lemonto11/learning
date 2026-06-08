3744c429953e6cb39b017d6864f500efc42207ac
Lemonto11 <564173367@qq.com>
2026-06-08 11:50:02 -0400

    标准IO的基本使用

--- c/part7/standard/basic.c
+++ c/part7/standard/basic.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
-
+#define SIZE 512
 int main(){
 
 	//fopen
-	FILE* fp = fopen("basic.c", "r+");
-	
+	FILE* fp = fopen("text.txt", "r+");
+	if(fp==NULL){
+		perror("fopen");
+		return -1;
+	}
 	//getchar()
+	printf("--------getchar-------\n");
 	char c;
 	while((c=getchar())!='\n'){
 		printf("%c", c);
@@ -14,14 +18,45 @@ int main(){
 	printf("\n");
 	
 	//fgetc()
+	printf("--------fgetc-------\n");
 	char ch;
 	while((ch = fgetc(fp))!=EOF){
 		printf("%c", ch);
 	}
 	printf("\n");
+	
+	//fgets(char *s, int size, FILE* stream)
+	printf("--------fgets-------\n");
+	rewind(fp);
+	char buf[SIZE];
+	int line = 0;
+	while(fgets(buf, SIZE, fp)!=NULL){
+		line++;
+		printf("%s", buf);
+	}
+	printf("一共有%d行\n", line);
+	
+	//fputs(const char *s, FILE* stream)
+	printf("--------fputs-------\n");
+	if(fputs("susususupernova", fp)==EOF){
+		perror("fputs");
+	}
+	printf("写入susususupernova\n");
+	
+	//fread(void* ptr, size_t size, size_t num, FILE* Stream)
+	printf("--------fread-------\n");
+	fseek(fp, 0, SEEK_SET);
+	char buffer[SIZE]={0};
+	size_t ret = fread(buffer, 1, SIZE-1, fp);
+	if(!ret){
+		perror("fread");
+		return -1;
+	}
+	buffer[SIZE]='\0';
+	printf("%s", buffer);
+	printf("\n");
+	
 	//fclose
 	fclose(fp);
     return 0;
 }
-
-
